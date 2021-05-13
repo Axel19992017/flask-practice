a6b4357abf16039c87d818672a996fff3ea6eca6
CarlosDuarte17 <carlosad154@gmail.com>
2021-05-13 15:36:26 +0000

    🐛 Corrigiendo errores en anotaciones.py

--- routes/anotaciones.py
+++ routes/anotaciones.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash
+from flask import render_template, flash, session
 
 from app import app, db, login_required
 
@@ -6,13 +6,13 @@ from app import app, db, login_required
 @login_required
 def anotaciones():
 
-    consulta = db.execute("SELECT * FROM  anotaciones WHERE id_usuario = :id ",
+    consulta = db.execute("SELECT * FROM anotacion WHERE id_usuario = :id ",
                             id = session["id_user"])
     return render_template("anotaciones/index.html", anotaciones=consulta)
 
 @app.route('/agregar')
 def agregar():
-    if request.method == "POST"
+    if request.method == "POST":
         titulo = request.form.get("titulo")
         contenido = request.form.get("contenido")
 
@@ -22,15 +22,14 @@ def agregar():
         if not request.form.get("contenido"):
             return apology("invalid content")
 
-        rows = db.execute("INSERT INTO anotaciones (id_usuario, titulo, contenido) VALUES (:id_usuario, :titulo, :contenido)",
+        rows = db.execute("INSERT INTO anotacion (id_usuario, titulo, contenido) VALUES (:id_usuario, :titulo, :contenido)",
                             id_usuario = session["id_user"], titulo = titulo, contenido = contenido)
         return redirect("/anotaciones")
 
 @app.route('/actualizar')
 def actualizar():
 
-    if request.method == "POST"
-
+    if request.method == "POST":
         id = request.form.get("id")
         titulo = request.form.get("titulo")
         contenido = request.form.get("contenido")
@@ -44,21 +43,20 @@ def actualizar():
         if not request.form.get("contenido"):
             return apology("invalid content")
 
-        rows = db.execute("UPDATE anotaciones SET titulo = :titulo, contenido = :contenido WHERE id = :id",
+        rows = db.execute("UPDATE anotacion SET titulo = :titulo, contenido = :contenido WHERE id = :id",
                             titulo = titulo, contenido = contenido, id = id)
         return redirect("/anotaciones")
 
 @app.route('/eliminar')
 def eliminar():
 
-    if request.method == "POST"
-
+    if request.method == "POST":
         id = request.form.get("id")
 
         if not request.form.get("id"):
             return apology("invalid id",403)
 
-        rows = db.execute("DELETE FROM anotaciones WHERE id = :id",
+        rows = db.execute("DELETE FROM anotacion WHERE id = :id",
                             id = id)
         return redirect("/anotaciones")
 
