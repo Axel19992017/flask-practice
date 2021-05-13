836d385001bea37137cf9c71db861a10007e26fc
AlexMartz <martinezalexanderantonio@hotmail.com>
2021-05-13 15:13:13 +0000

    :seedling: Agregando contatos #4

--- routes/contactos.py
+++ routes/contactos.py
@@ -1,8 +1,16 @@
-from flask import render_template
+from flask import Flask, flash, redirect, render_template, request, session
 
 from app import app, db, login_required
 
 @app.route('/contactos')
 @login_required
 def contactos():
-   return render_template("contactos/index.html")
+    if request.method == "POST":
+        nombre = request.form.get("nombre")
+        numero = request.form.get("numero")
+        correo = request.form.get("correo")
+        
+        db.execute(f"insert into contactos values(,,)")
+
+    contactos = db.execute("select * from contactos where id_usuario =:Id", Id=session["id_user"])
+    return render_template("contactos/index.html",contactos = contactos)
